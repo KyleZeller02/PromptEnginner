d5b997db779483a9257d0adc54f55fb2cd6d283c
kzeller02 <kzeller02@ksu.edu>
2025-06-30 17:56:22 -0500

    Initial Commit

---
+++ PromptEnginner/PromptEnginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptEnginnerApp.swift
+//  PromptEnginner
+//
+//  Created by Kyle Zeller on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PromptEnginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptEnginnerTests/PromptEnginnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PromptEnginnerTests.swift
+//  PromptEnginnerTests
+//
+//  Created by Kyle Zeller on 6/30/25.
+//
+
+import Testing
+@testable import PromptEnginner
+
+struct PromptEnginnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
